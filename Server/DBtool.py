53d6c0c2506addff02aab1b4b2a91672dd3ba56d
ylxdzsw <ylxdzsw@gmail.com>
2014-07-07 09:29:19 +0800

    为了pull

--- Server/DBtool.py
+++ Server/DBtool.py
@@ -14,25 +14,30 @@ def index():
 	a = lambda x: forall(ensureindex(x))
 	return a('time'),a('daytime'),a('hourtime')
 def archive():
-	today,nowhour = getday(time.time()),gethour(time.time())
-	p = {}
+	for i in ['daytime','hourtime']: archiveall(i)
+#small funcs
+def archiveall(x):
+	now = {'daytime':getday(time.time()),'hourtime':gethour(time.time())}
+	p={}
+	temp = {'hourtime':'none','daytime':'hour'}
+	temp = temp[x]
 	for k in load.getallname():
-		for i in load.popby(k,'hourtime',{'archivelabel':'none'}):
+		for i in load.popby(k,x,{'archivelabel':temp}):
 			for j in i:
 				for l in ['CPU','IO_write','IO_read','MainMemory']:
-					p[l] = p.get(l,0) + i[l]
+					p[l] = p.get(l,0) + j[l]
 					p[l,0] = p.get((l,0),0) + 1
 			for l in ['CPU','IO_write','IO_read','MainMemory']:
 				p[l] = p[l] / p[l,0]
 				p.pop((l,0))
-			p['hourtime'] = j['hourtime']
-			p['archivelabel'] = 'hour'
+			p[x] = j[x]
+			if now[x] == p[x]:
+				p.clear()
+				continue
+			p['archivelabel'] = x[:-4]
 			p['Id'] = k
 			save.insert(p)
 			p.clear()
-				
-
-#small funcs
 def forall(f):
 	return [f(col) for col in (db[name] for name in db.collection_names() if name not in ['system.indexes'])]
 def ensureindex(colname):
